7163cf07b7e927ef747f7da08746247633d3e020
djzhang <trujunzhang@gmail.com>
2020-05-10 10:48:57 +0800

    geoflutterfire

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,256 @@
 import 'package:flutter/material.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'streambuilder_test.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:rxdart/rxdart.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      title: 'Geo Flutter Fire example',
+      home: MyApp(),
+      debugShowCheckedModeBanner: false,
+    ));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  GoogleMapController _mapController;
+  TextEditingController _latitudeController, _longitudeController;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  // firestore init
+  Firestore _firestore = Firestore.instance;
+  Geoflutterfire geo;
+  Stream<List<DocumentSnapshot>> stream;
+  var radius = BehaviorSubject<double>.seeded(1.0);
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  void initState() {
+    super.initState();
+    _latitudeController = TextEditingController();
+    _longitudeController = TextEditingController();
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    geo = Geoflutterfire();
+    GeoFirePoint center = geo.point(latitude: 12.960632, longitude: 77.641603);
+    stream = radius.switchMap((rad) {
+      var collectionReference = _firestore.collection('locations');
+//          .where('name', isEqualTo: 'darshan');
+      return geo.collection(collectionRef: collectionReference).within(
+          center: center, radius: rad, field: 'position', strictMode: true);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      /*
+      ****Example to specify nested object****
+
+      var collectionReference = _firestore.collection('nestedLocations');
+//          .where('name', isEqualTo: 'darshan');
+      return geo.collection(collectionRef: collectionReference).within(
+          center: center, radius: rad, field: 'address.location.position');
+
+      */
     });
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    radius.close();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('GeoFlutterFire'),
+          actions: <Widget>[
+            IconButton(
+              onPressed: _mapController == null
+                  ? null
+                  : () {
+                      _showHome();
+                    },
+              icon: Icon(Icons.home),
+            )
           ],
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+              return StreamTestWidget();
+            }));
+          },
+          child: Icon(Icons.navigate_next),
+        ),
+        body: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Card(
+                  elevation: 4,
+                  margin: EdgeInsets.symmetric(vertical: 8),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width - 30,
+                    height: MediaQuery.of(context).size.height * (1 / 3),
+                    child: GoogleMap(
+                      onMapCreated: _onMapCreated,
+                      initialCameraPosition: const CameraPosition(
+                        target: LatLng(12.960632, 77.641603),
+                        zoom: 15.0,
+                      ),
+                      markers: Set<Marker>.of(markers.values),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Slider(
+                  min: 1,
+                  max: 200,
+                  divisions: 4,
+                  value: _value,
+                  label: _label,
+                  activeColor: Colors.blue,
+                  inactiveColor: Colors.blue.withOpacity(0.2),
+                  onChanged: (double value) => changed(value),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    child: TextField(
+                      controller: _latitudeController,
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.next,
+                      decoration: InputDecoration(
+                          labelText: 'lat',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          )),
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    child: TextField(
+                      controller: _longitudeController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                          labelText: 'lng',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          )),
+                    ),
+                  ),
+                  MaterialButton(
+                    color: Colors.blue,
+                    onPressed: () {
+                      double lat = double.parse(_latitudeController.text);
+                      double lng = double.parse(_longitudeController.text);
+                      _addPoint(lat, lng);
+                    },
+                    child: Text(
+                      'ADD',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+              MaterialButton(
+                color: Colors.amber,
+                child: Text(
+                  'Add nested ',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  double lat = double.parse(_latitudeController.text);
+                  double lng = double.parse(_longitudeController.text);
+                  _addNestedPoint(lat, lng);
+                },
+              )
+            ],
+          ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      _mapController = controller;
+//      _showHome();
+      //start listening after map is created
+      stream.listen((List<DocumentSnapshot> documentList) {
+        _updateMarkers(documentList);
+      });
+    });
+  }
+
+  void _showHome() {
+    _mapController.animateCamera(CameraUpdate.newCameraPosition(
+      const CameraPosition(
+        target: LatLng(12.960632, 77.641603),
+        zoom: 15.0,
+      ),
+    ));
+  }
+
+  void _addPoint(double lat, double lng) {
+    GeoFirePoint geoFirePoint = geo.point(latitude: lat, longitude: lng);
+    _firestore
+        .collection('locations')
+        .add({'name': 'random name', 'position': geoFirePoint.data}).then((_) {
+      print('added ${geoFirePoint.hash} successfully');
+    });
+  }
+
+  //example to add geoFirePoint inside nested object
+  void _addNestedPoint(double lat, double lng) {
+    GeoFirePoint geoFirePoint = geo.point(latitude: lat, longitude: lng);
+    _firestore.collection('nestedLocations').add({
+      'name': 'random name',
+      'address': {
+        'location': {'position': geoFirePoint.data}
+      }
+    }).then((_) {
+      print('added ${geoFirePoint.hash} successfully');
+    });
+  }
+
+  void _addMarker(double lat, double lng) {
+    MarkerId id = MarkerId(lat.toString() + lng.toString());
+    Marker _marker = Marker(
+      markerId: id,
+      position: LatLng(lat, lng),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+      infoWindow: InfoWindow(title: 'latLng', snippet: '$lat,$lng'),
+    );
+    setState(() {
+      markers[id] = _marker;
+    });
+  }
+
+  void _updateMarkers(List<DocumentSnapshot> documentList) {
+    documentList.forEach((DocumentSnapshot document) {
+      GeoPoint point = document.data['position']['geopoint'];
+      _addMarker(point.latitude, point.longitude);
+    });
+  }
+
+  double _value = 20.0;
+  String _label = '';
+
+  changed(value) {
+    setState(() {
+      _value = value;
+      _label = '${_value.toInt().toString()} kms';
+      markers.clear();
+    });
+    radius.add(value);
   }
 }
